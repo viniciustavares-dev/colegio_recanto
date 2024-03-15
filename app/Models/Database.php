8241c76f1a02e19aedb7b2b22209e90b97232b69
Vinicius Tavares <siqueiravinicius@yahoo.com.br>
2024-03-15 12:19:09 -0300

    adiciona verificação de conexão

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -12,15 +12,18 @@ class Database {
     private $conn;
 
     public function getConnection() {
-        $this->conn = null;
-
+        if ($this->conn !== null) {
+            return $this->conn;
+        }
+    
         try {
             $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo 'Erro de conexão: ' . $e->getMessage();
         }
-
+    
         return $this->conn;
     }
+    
 }
